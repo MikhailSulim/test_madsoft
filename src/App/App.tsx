8ca9e797c7689e95451fb40f3a8aba9b2cae823b
Mikhail Sulim <mika-1984@yandex.ru>
2024-06-03 17:43:00 +0300

    refactor: prepare for deploy

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,28 +1,32 @@
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
-import data from './../data/questions.json';
 import TestForm from '../components/TestForm/TestForm';
+import { IData } from '../utils/types';
 
 function App() {
-  // const [data, setData] = useState<Data | null>(null);
-  // useEffect(() => {
-  //   fetch('/public/data/questions.json')
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error('Network response was not ok');
-  //       }
+  const [data, setData] = useState<IData | null>(null);
+  useEffect(() => {
+    fetch('data/questions.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
 
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       setData(data);
-  //     })
-  //     .catch((error) => console.error(error));
-  // }, []);
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <div className="App">
-      <TestForm questions={data.questions} testTime={data.testTime} />
+      {data ? (
+        <TestForm questions={data.questions} testTime={data.testTime} />
+      ) : (
+        'Идёт загрузка...'
+      )}
     </div>
   );
 }
